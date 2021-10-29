6516bac688a2cbe3d8fda030f559655e6ef8b3e0
Adenilson Silva <adenilson@telluria.com.br>
2021-10-29 09:13:42 -0300

    feat(all): initial commit

---
+++ src/hooks/useGetter/useGetter.ts
@@ -0,0 +1,13 @@
+import { useRef, useCallback, useEffect } from 'react';
+
+export default function useGetter<T>(value: T): () => T {
+  const valueRef = useRef(value);
+
+  useEffect(() => {
+    valueRef.current = value;
+  }, [value]);
+
+  const getValue = useCallback(() => valueRef.current, []);
+
+  return getValue;
+}
